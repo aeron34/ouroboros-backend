ef67cc2486ac049b5dbd64373a275cfbca2de141
aeron34 <sniperud@gmail.com>
2025-09-22 21:29:31 -0400

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express();
+require('dotenv').config();
+
+router.use(express.urlencoded({extended: false}));
+router.use(express.json());
+
+const paths = ['user']
+
+/* These routes will each be turned into microservices in the future */
+paths.map(path => router.use(`/${path.split('/')[0]}`, require(`./features/${path}`)))
+
+router.get('/', (req, res) => {
+  res.status(300).send('default route.')
+})
+
+router.listen(process.env.PORT);
